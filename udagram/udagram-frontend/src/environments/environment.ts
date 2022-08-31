b624120adba11e50edcd053911a4ddec795b3a69
Andrew Boshra <andrew.boshr@gmail.com>
2022-09-01 00:40:38 +0200

    changed api url

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -3,9 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+    production: false,
+    appName: "Udagram",
+    apiHost:
+        "Udagramapidev-env.eba-gaa2uygg.us-west-2.elasticbeanstalk.com/api/v0",
 };
 
 /*
