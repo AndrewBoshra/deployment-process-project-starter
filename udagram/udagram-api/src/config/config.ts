62b930ad30d75319a0d448ab17a8072142e0bdaa
Andrew Boshra <andrew.boshr@gmail.com>
2022-08-31 23:37:48 +0200

    changed env names

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -7,17 +7,17 @@ dotenv.config();
 // Are Also needed
 
 export const config = {
-  username: `${process.env.POSTGRES_USERNAME}`,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
-  port: Number(process.env.PORT),
-  host: process.env.POSTGRES_HOST,
-  dialect: "postgres",
-  aws_region: process.env.AWS_REGION,
-  aws_profile: process.env.AWS_PROFILE,
-  aws_media_bucket: process.env.AWS_BUCKET,
-  url: process.env.URL,
-  jwt: {
-    secret: process.env.JWT_SECRET,
-  },
+    username: `${process.env.RDS_USERNAME}`,
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_DB,
+    port: Number(process.env.PORT),
+    host: process.env.RDS_HOSTNAME,
+    dialect: "postgres",
+    aws_region: process.env.AWS_REGION,
+    aws_profile: process.env.AWS_PROFILE,
+    aws_media_bucket: process.env.AWS_BUCKET,
+    url: process.env.URL,
+    jwt: {
+        secret: process.env.JWT_SECRET,
+    },
 };
